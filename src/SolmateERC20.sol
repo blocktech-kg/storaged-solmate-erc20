2b1126ad605e09390633654060846d80696b9a5c
stas <rontero@gmail.com>
2025-04-23 18:57:35 +0600

    add permit according to EIP-2612

--- src/SolmateERC20.sol
+++ src/SolmateERC20.sol
@@ -28,6 +28,10 @@ contract SolmateERC20 {
         return ERC20StorageLib.get().decimals;
     }
 
+    function nonces(address owner) public view virtual returns (uint256) {
+        return ERC20StorageLib.get().nonces[owner];
+    }
+
     /*//////////////////////////////////////////////////////////////
                               ERC20 VIEW FUNCTIONS
     //////////////////////////////////////////////////////////////*/
@@ -49,19 +53,19 @@ contract SolmateERC20 {
     //////////////////////////////////////////////////////////////*/
 
     function approve(address spender, uint256 amount) public virtual returns (bool) {
-        ERC20StorageLib.ERC20Storage storage s = ERC20StorageLib.get();
-        s.allowance[msg.sender][spender] = amount;
+        ERC20StorageLib.ERC20Storage storage $ = ERC20StorageLib.get();
+        $.allowance[msg.sender][spender] = amount;
 
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
     function transfer(address to, uint256 amount) public virtual returns (bool) {
-        ERC20StorageLib.ERC20Storage storage s = ERC20StorageLib.get();
-        s.balanceOf[msg.sender] -= amount;
+        ERC20StorageLib.ERC20Storage storage $ = ERC20StorageLib.get();
+        $.balanceOf[msg.sender] -= amount;
 
         unchecked {
-            s.balanceOf[to] += amount;
+            $.balanceOf[to] += amount;
         }
 
         emit Transfer(msg.sender, to, amount);
@@ -69,15 +73,15 @@ contract SolmateERC20 {
     }
 
     function transferFrom(address from, address to, uint256 amount) public virtual returns (bool) {
-        ERC20StorageLib.ERC20Storage storage s = ERC20StorageLib.get();
-        uint256 allowed = s.allowance[from][msg.sender];
+        ERC20StorageLib.ERC20Storage storage $ = ERC20StorageLib.get();
+        uint256 allowed = $.allowance[from][msg.sender];
 
-        if (allowed != type(uint256).max) s.allowance[from][msg.sender] = allowed - amount;
+        if (allowed != type(uint256).max) $.allowance[from][msg.sender] = allowed - amount;
 
-        s.balanceOf[from] -= amount;
+        $.balanceOf[from] -= amount;
 
         unchecked {
-            s.balanceOf[to] += amount;
+            $.balanceOf[to] += amount;
         }
 
         emit Transfer(from, to, amount);
@@ -85,35 +89,78 @@ contract SolmateERC20 {
     }
 
     /*//////////////////////////////////////////////////////////////
-                         INTERNAL MINT LOGIC
+                         MINT/BURN LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function mint(address to, uint256 amount) external virtual {
-        ERC20StorageLib.ERC20Storage storage s = ERC20StorageLib.get();
-        s.totalSupply += amount;
-
-        unchecked {
-            s.balanceOf[to] += amount;
-        }
+    function mint(address to, uint256 amount) public virtual {
+        ERC20StorageLib.ERC20Storage storage $ = ERC20StorageLib.get();
+        $.balanceOf[to] += amount;
+        $.totalSupply += amount;
 
         emit Transfer(address(0), to, amount);
+    }
+
+    function burn(address from, uint256 amount) public virtual {
+        ERC20StorageLib.ERC20Storage storage $ = ERC20StorageLib.get();
+
+        $.balanceOf[from] -= amount;
+        $.totalSupply -= amount;
+
+        emit Transfer(from, address(0), amount);
     }
 
     /*//////////////////////////////////////////////////////////////
                              EIP-2612 LOGIC
     //////////////////////////////////////////////////////////////*/
 
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external virtual {
+        ERC20StorageLib.ERC20Storage storage $ = ERC20StorageLib.get();
+
+        require(block.timestamp <= deadline, "PERMIT_DEADLINE_EXPIRED");
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR(),
+                keccak256(
+                    abi.encode(
+                        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"),
+                        owner,
+                        spender,
+                        value,
+                        $.nonces[owner]++,
+                        deadline
+                    )
+                )
+            )
+        );
+
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == owner, "INVALID_PERMIT_SIGNATURE");
+
+        $.allowance[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
     function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
-        ERC20StorageLib.ERC20Storage storage s = ERC20StorageLib.get();
-        return block.chainid == s.INITIAL_CHAIN_ID ? s.INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
+        ERC20StorageLib.ERC20Storage storage $ = ERC20StorageLib.get();
+        return block.chainid == $.INITIAL_CHAIN_ID ? $.INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
     }
 
     function computeDomainSeparator() internal view virtual returns (bytes32) {
-        ERC20StorageLib.ERC20Storage storage s = ERC20StorageLib.get();
+        ERC20StorageLib.ERC20Storage storage $ = ERC20StorageLib.get();
         return keccak256(
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(bytes(s.name)),
+                keccak256(bytes($.name)),
                 keccak256("1"),
                 block.chainid,
                 address(this)
